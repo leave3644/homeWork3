f529203481ec4ee7797a3965fcdf6f314f7a1e0b
Kaili An <kailian@bussticktry-lm.corp.yahoo.com>
2014-06-17 01:06:09 -0700

    Add profile page
    User is able to click profile picture and direct to profile page

---
+++ homeWork3/profileViewController.h
@@ -0,0 +1,13 @@
+//
+//  profileViewController.h
+//  homeWork3
+//
+//  Created by Kaili An on 6/17/14.
+//  Copyright (c) 2014 Kelly An. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface profileViewController : UIViewController
+
+@end
--- homeWork3/profileViewController.m
+++ homeWork3/profileViewController.m
@@ -1,20 +1,18 @@
 //
-//  moreViewController.m
+//  profileViewController.m
 //  homeWork3
 //
 //  Created by Kaili An on 6/17/14.
 //  Copyright (c) 2014 Kelly An. All rights reserved.
 //
 
-#import "moreViewController.h"
 #import "profileViewController.h"
 
-@interface moreViewController ()
-- (IBAction)profileButton:(id)sender;
+@interface profileViewController ()
 
 @end
 
-@implementation moreViewController
+@implementation profileViewController
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -37,10 +35,4 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)profileButton:(id)sender {
-    UIViewController *vc = [[profileViewController alloc]init];
-    vc.modalTransitionStyle = UIModalTransitionStyleFlipHorizontal;
-    
-    [self presentViewController:vc animated:YES completion:nil];
-}
 @end
